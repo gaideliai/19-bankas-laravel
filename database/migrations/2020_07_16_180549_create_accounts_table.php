7ec0b06e297a0c3eab2534fae4d3b337ecedde80
gaideliai <61122798+gaideliai@users.noreply.github.com>
2020-07-16 22:26:02 +0300

    initial commit

---
+++ database/migrations/2020_07_16_180549_create_accounts_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateAccountsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('accounts', function (Blueprint $table) {
+            $table->id();
+            $table->string('account', 20);
+            $table->decimal('balance', 16, 2);
+            $table->timestamps();
+            $table->unsignedBigInteger('client_id');
+            $table->foreign('client_id')->references('id')->on('clients');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('accounts');
+    }
+}
